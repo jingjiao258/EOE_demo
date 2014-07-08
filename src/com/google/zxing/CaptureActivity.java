ecadcdef6811caa54426a234a1f5c96bfe7cfa51
jingjiao258 <1035766456@qq.com>
2014-07-08 14:28:55 +0800

    Update CaptureActivity.java

--- src/com/google/zxing/CaptureActivity.java
+++ src/com/google/zxing/CaptureActivity.java
@@ -63,7 +63,7 @@ public class CaptureActivity extends Activity implements Callback {
 
         CameraManager.init(getApplication());
         initControl();
-
+           
         hasSurface = false;
         inactivityTimer = new InactivityTimer(this);
     }
